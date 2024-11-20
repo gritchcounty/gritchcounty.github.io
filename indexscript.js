47f34e69c7c5b806f70164cfb4affd63d51164ec
gritchcounty <compacktdisck@protonmail.com>
2024-11-20 18:11:38 -0500

    Updated js

--- indexscript.js
+++ indexscript.js
@@ -1,13 +1,16 @@
 var n = new Date();
-var m = n.getMonth() + 1;
-var y = n.getFullYear();
-if (n.getDate > 12) {
-    m++;
-}
-
-if (n.getDate() == 12) {
-    document.write(m + "/12/" + y)
-    document.write("<span style='font-family = figtree-ital'>TODAY</span>");
+if (n.getMonth() == 11) {
+    var nextBill = new Date(n.getFullYear() + 1, 0, 12)
 } else {
-    document.write(m + "/12/" + y);
+    var nextBill = new Date(n.getFullYear(), n.getMonth() + 1, 12)
+}
+var d = nextBill.getDate();
+var m = nextBill.getMonth() + 1;
+var y = nextBill.getFullYear();
+if (n.getDate() == 12) {
+    document.write("<span style='font-family = figtree-ital'>TODAY</span>");
+} else  if (n. getDate > 12) {
+    document.write(n.getMonth() + "/12" + "/" + n.getFullYear())
+} else {
+    document.write(m + "/" + d + "/" + y);
 }
