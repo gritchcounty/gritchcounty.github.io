d921bec6af9b6c44f88597f5a320e4c22bdb897e
gritchcounty <compacktdisck@protonmail.com>
2024-12-01 01:07:50 -0500

    added Sudo Padre info

--- indexscript.js
+++ indexscript.js
@@ -1,16 +1,15 @@
 var n = new Date();
-if (n.getMonth() == 11) {
-    var nextBill = new Date(n.getFullYear() + 1, 0, 12)
-} else {
-    var nextBill = new Date(n.getFullYear(), n.getMonth() + 1, 12)
-}
-var d = nextBill.getDate();
-var m = nextBill.getMonth() + 1;
-var y = nextBill.getFullYear();
 if (n.getDate() == 12) {
-    document.write("<span style='font-family = figtree-ital'>TODAY</span>");
-} else  if (n. getDate > 12) {
-    document.write(n.getMonth() + "/12" + "/" + n.getFullYear())
+    document.write("<span style='font-family: figtree-blk; font-style: italic'>TODAY</span>");
+} else  if (n.getDate() > 12) {
+    if (n.getMonth() == 11) {
+        var nextBill = new Date(n.getFullYear() + 1, 1, 12);
+    } else {
+        var nextBill = new Date(n.getFullYear(), n.getMonth() + 2, 12);
+    }
+    document.write("on " + nextBill.getMonth() + "/12/" + nextBill.getFullYear());
 } else {
-    document.write(m + "/" + d + "/" + y);
+    var m = n.getMonth() + 1;
+    var y = n.getFullYear();
+    document.write("on " + m + "/12/" + y);
 }
